c6dfc816fb5599a84b77010e19ebdc0669771553
Danijel Baltić <danijel.baltic@outlook.com>
2025-11-08 14:23:39 +0100

    Booking form is visualy ready, other visual errors are done

---
+++ Components/OpenPopUp.cs
@@ -0,0 +1,12 @@
+﻿namespace TaxiBoatBaltic.Components
+{
+    public class OpenPopUp
+    {
+        private BookingForm bookingPopup;
+
+        private void ShowPopup()
+        {
+            bookingPopup.Show();
+        }
+    }
+}
